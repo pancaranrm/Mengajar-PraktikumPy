9264ffb51ea1fe20daf65e38bc7805340ad30510
hyprll <ancaanca297@gmail.com@gmail.com>
2022-08-27 21:17:37 +0700

    tambahin latihan while

---
+++ latihan/latihan_while.py
@@ -0,0 +1,44 @@
+# print("# 𝙉𝙖𝙩𝙪𝙧𝙖𝙡 𝙣𝙪𝙢𝙗𝙚𝙧")
+# # Write a program to print sum of first 10 Natural numbers
+# num = 1
+# while num <= 10:
+#     print(num)
+#     num += 1
+
+# SOAL 2
+# print("# 𝙍𝙚𝙫𝙚𝙧𝙨𝙚 𝙉𝙖𝙩𝙪𝙧𝙖𝙡")
+# # Write a program to print first 10 natural number in reverse order using while loop.
+# # 10 kebalik
+# num = 10 #biar kebalik okey!
+# while (num >= 1):
+#     print(num)
+#     num -= 1
+
+# SOAL 3
+# #  buat tabel lalu pengulangan 1-10 hasilnya adalah *2
+# num = 1
+# print("Numbers \t Result")
+
+# while (num <= 10):
+#     print(num, "\t\t\t", num *2)
+#     num += 1
+# print("\t END \t")
+
+# SOAL 4
+# Write a program to print sum of first 10 Even numbers.
+sum = 2
+num = 0
+while sum <= 20 :
+    sum += num
+    num += 2
+    print(sum)
+
+
+sum = 0
+for i in range(1,11):
+    print(i)
+    sum+=1
+print(sum)
+
+
+
