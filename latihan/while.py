3755a2393ceb94efaeb7d903ae8b96288a63ebf8
hyprll <ancaanca297@gmail.com@gmail.com>
2022-08-28 10:23:12 +0700

    nambahin latihan while

--- latihan/while.py
+++ latihan/while.py
@@ -1,3 +1,6 @@
+from xml.dom.pulldom import END_DOCUMENT
+
+
 # print("# 𝙉𝙖𝙩𝙪𝙧𝙖𝙡 𝙣𝙪𝙢𝙗𝙚𝙧")
 # # Write a program to print sum of first 10 Natural numbers
 # num = 1
@@ -5,17 +8,17 @@
 #     print(num)
 #     num += 1
 
-# SOAL 2
+# # question 2
 # print("# 𝙍𝙚𝙫𝙚𝙧𝙨𝙚 𝙉𝙖𝙩𝙪𝙧𝙖𝙡")
 # # Write a program to print first 10 natural number in reverse order using while loop.
-# # 10 kebalik
+
 # num = 10 #biar kebalik okey!
 # while (num >= 1):
 #     print(num)
 #     num -= 1
 
-# SOAL 3
-# #  buat tabel lalu pengulangan 1-10 hasilnya adalah *2
+# # question 3
+# #  
 # num = 1
 # print("Numbers \t Result")
 
@@ -24,21 +27,28 @@
 #     num += 1
 # print("\t END \t")
 
-# SOAL 4
-# Write a program to print sum of first 10 Even numbers.
-sum = 2
-num = 0
-while sum <= 20 :
-    sum += num
-    num += 2
-    print(sum)
+# # question 4
+# # Write a program to print sum of first 10 Even numbers.
+# sum = 2
+# num = 0
+# while sum <= 20 :
+#     sum += num
+#     num += 2
+#     print(sum)
 
 
-sum = 0
-for i in range(1,11):
-    print(i)
-    sum+=1
-print(sum)
+# sum = 0
+# for i in range(1,11):
+#     print(i)
+#     sum+=1
+# print(sum)
 
+#  Write a program to print table of a number entered from the user.
+# buatlah program perkalian yang dimasukkan user
 
+i = 1
+b = int(input("Masukkan inputan angka pilihanmu"))
 
+while i <= 10:
+    print(b, " *", i , "=", b*i)
+    i += 1
