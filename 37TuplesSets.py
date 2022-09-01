a81339c986579c726813b624a562a1e42a390d63
hyprll <ancaanca297@gmail.com@gmail.com>
2022-09-01 20:44:12 +0700

    add tuples,dic

---
+++ 37TuplesSets.py
@@ -0,0 +1,14 @@
+# Tuples () untuk fixed content tidak bisa diedit dan ditambahkan,hapus 
+# Sets/himpunan {1,2,3,3} semuanya bisa tapi ga bisa diindein kek []
+
+tuples = (1,2,3,4)
+setss = {1,2,3,4,5}
+
+print("Data : ",tuples)
+print("Data : ",setss)
+
+# tidak bisa dilakukan
+# tuples[1]= 4
+# print("Yang baru",tuples)\
+
+print(setss[1])# ga bisa
---
+++ 38Dictionary.py
@@ -0,0 +1,16 @@
+# Dictionary// kamus digunakan untuk mempersingkat dan mengaliaskan anjay gmn jelasinnya 
+# disebut juga associative array 
+# identicier : key
+
+proyektor = ['Silinder','kerucut','kutub']
+print(proyektor)
+
+data_dict = {
+    'Carl Marx':'Marxisme',
+    'Max waber' : 'Kapitalisme-cita',
+    'Ferdinand Tonnies' : 'Kelompok pengayuban & petambayan',
+    'Chc' : 'Glass effect',
+    'pyk' : proyektor, 
+}
+print(data_dict['Chc'])
+print(data_dict['pyk'])
