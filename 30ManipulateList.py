2977429a6b15e8b10410b4f5323c874576770a16
hyprll <ancaanca297@gmail.com@gmail.com>
2022-09-01 13:08:53 +0700

    tambahin manipulasi,list

---
+++ 30ManipulateList.py
@@ -0,0 +1,41 @@
+from turtle import pen
+
+
+data = ['A','B','C']
+
+# total data list
+total = len(data)
+print(total)
+
+# memanipulasi per list
+list = data[2]
+print("Outputnya adalah..",list)
+
+# mengambil data terakhir
+akhir = data[-1]
+print("List data terakhir",akhir)
+
+# menambah data pada list tertentu
+data.insert(1,"Jajang")
+print(data)
+
+# menambah di akhir list
+data.append("tambah akhir")
+print("Data ditmbah lagi",data)
+
+# menggabungkan list
+data_baru = ['Anca','okr','cape']
+data.extend(data_baru)
+print("Data baru",data)
+
+# merubah data
+data[1] = "Yaudah"
+print("Data diubah menjadi",data)
+
+# menghapus data 
+data.remove('cape')
+print(f"data {data} terhapus")
+
+# menghapus data paling belakang = pop
+data.pop()
+print(f"data terakhir dihapus jadinya tinggal.. {data}")
---
+++ 31List.py
@@ -0,0 +1,26 @@
+# cara menghitung data pada list yang panjang
+from tkinter import PIESLICE
+
+
+data = [1,2,1,2,1,3,2,1,3,4,5,1,0]
+# hitung angka 1 ada berapa
+jumlahData1 = data.count(4)
+print(f"Jumlah data angka 1 adalah... {jumlahData1} ")
+
+
+data = ["anca","oke","siap","kaya"]
+# cara ambil data pada list
+# INDEXING
+
+ambil = data.index("kaya")
+print(f"Kita mengambil data {ambil}")
+
+# cara mengurutkan data 
+print(f"Sebelum diurutkan = \n {data}")
+# setelah diurutkan
+data.sort()
+print(f"data sudah disort= \n {data}")
+
+# mereverse data 
+data.reverse()
+print(f"data sudah direverse= \n {data}")
