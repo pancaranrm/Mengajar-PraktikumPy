f66348169555923a5732e1b09dddd0e941092671
hyprll <ancaanca297@gmail.com@gmail.com>
2022-09-02 21:13:29 +0700

    bingung

--- 43LatihanDictionary.py
+++ 43LatihanDictionary.py
@@ -2,3 +2,32 @@
 
 import datetime
 import os
+# import random
+# import string
+
+datam = {
+	'nama':'nama',
+	'nim':'000000',
+	'sks_lulus':0,
+	'lahir':datetime.datetime(1111,1,11)
+}
+
+mhs = {}
+
+while True:
+    os.system("cls")
+    print("SELAMAT DATANG MAHASISWA")
+    print("Harap Isi Form Dibawah")
+    print("-"*50)
+
+    mahasiswa = dict.fromkeys(datam.keys())
+
+    mahasiswa['nama'] = str(input("Masukkan nama anda"))
+    mahasiswa['nim'] = int(input("Masukkan NIM anda"))
+    mahasiswa['sks_lulus'] = int(input("Masukkan SKS anda"))
+    TAHUN = int(input("Tahun lahir kamu"))
+    BULAN = int(input("Bulan lahir kamu"))
+    TANGGAL = int(input("Tanggal lahir kamu"))
+    mahasiswa['lahir'] = datetime.datetime(TAHUN,BULAN,TANGGAL)
+    print(mahasiswa)
+
