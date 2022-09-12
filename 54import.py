6a29a61e9a243208db465d3a7112f4ffd51e6e8a
hyprll <ancaanca297@gmail.com@gmail.com>
2022-09-12 20:29:55 +0700

    e 54

--- 54import.py
+++ 54import.py
@@ -1,9 +1,13 @@
 # fungsinya adalah utk mengambil program dari file eksternal
 # memanggil file lain
+# disebut dengan namespace untuk pemanggilannya
 
 # 1
 import hi # mengimport file hi.py
 
 # 2. Import dengan data
-import importlib
+import buatimpor
+#  cara ngambil datanya yakni
+print(buatimpor.dataku) #namaFile.namaVar
 
+print(buatimpor.tambah(10,8)) #hasilnya 10+8 = 18
--- buatimpor.py
+++ buatimpor.py
@@ -1,4 +1,5 @@
 # ini buat impor aja kok
 dataku = [1,2,3,4,5,6,7]
 
-print("Ini adalah program untuk diimport")
+def tambah(a:float, b:float)->float:
+    return a+b
